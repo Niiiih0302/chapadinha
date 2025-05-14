47f7f6812d1c4d24d423da13ac1263c834e2d0cd
NathanLucasVieira <nathanlucas858@gmail.com>
2025-05-14 12:27:26 -0300

    Criação da pagina de cards

---
+++ Paginas/PaginaCards.php
@@ -0,0 +1,83 @@
+<link rel="stylesheet" href="../Estilos/PagCardsEstilo.css">
+<?php 
+
+include '../includes/head.php';
+include '../includes/header.php';
+
+?>
+
+<body>
+<div class="card-container">
+        <div class="card">
+            <div class="content">
+                <div class="front">
+                    <!-- Dados do banco de dados: imagem e nome da árvore -->
+                    <img src="imagem-arvore1.jpg" alt="Árvore 1">
+                    <h3>Nome da Árvore 1</h3>
+                </div>
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="content">
+                <div class="front">
+                    <!-- Dados do banco de dados: imagem e nome da árvore -->
+                    <img src="imagem-arvore2.jpg" alt="Árvore 2">
+                    <h3>Nome da Árvore 2</h3>
+                </div>
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="content">
+                <div class="front">
+                    <!-- Dados do banco de dados: imagem e nome da árvore -->
+                    <img src="imagem-arvore3.jpg" alt="Árvore 3">
+                    <h3>Nome da Árvore 3</h3>
+                </div>
+            </div>
+        </div>
+
+        
+
+     
+
+    </div>
+    <div class="card-container">
+        <div class="card">
+            <div class="content">
+                <div class="front">
+                    <!-- Dados do banco de dados: imagem e nome da árvore -->
+                    <img src="imagem-arvore1.jpg" alt="Árvore 1">
+                    <h3>Nome da Árvore 1</h3>
+                </div>
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="content">
+                <div class="front">
+                    <!-- Dados do banco de dados: imagem e nome da árvore -->
+                    <img src="imagem-arvore2.jpg" alt="Árvore 2">
+                    <h3>Nome da Árvore 2</h3>
+                </div>
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="content">
+                <div class="front">
+                    <!-- Dados do banco de dados: imagem e nome da árvore -->
+                    <img src="imagem-arvore3.jpg" alt="Árvore 3">
+                    <h3>Nome da Árvore 3</h3>
+                </div>
+            </div>
+        </div>
+
+        
+
+     
+
+    </div>
+</body>
+<?php include '../includes/footer.php'; ?>
