9173a3da2c8cc932a58cf57f4832b113dc1b79a4
MarcosSiqueiraSantos <marcosiqueira04@gmail.com>
2025-05-19 12:53:00 -0300

    Edição Header

--- includes/header.php
+++ includes/header.php
@@ -4,10 +4,15 @@
 <header class="site-header">
   <div class="container">
     <div class="header-content">
+      <!-- Logo -->
       <div class="logo-text">Lagoa da Chapadinha</div>
+      
+      <!-- Menu de navegação -->
       <nav class="nav-menu">
         <a href="index.php">Início</a>
-        <!-- Adicione os outros 3 links aqui -->
+        <a href="#">Sobre</a>
+        <a href="#">Serviços</a>
+        <a href="#">Contato</a>
       </nav>
     </div>
   </div>
