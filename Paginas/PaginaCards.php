d45143cb9aad8de8886a3f562230d67af61a63b1
MarcosSiqueiraSantos <marcosiqueira04@gmail.com>
2025-05-21 12:19:36 -0300

    Atualizado Cards e seu css

--- Paginas/PaginaCards.php
+++ Paginas/PaginaCards.php
@@ -1,53 +1,17 @@
-<link rel="stylesheet" href="../Estilos/PagCardsEstilo.css">
 <?php 
-
 include '../includes/head.php';
-include '../includes/header.php';
-
 ?>
 
+<link rel="stylesheet" href="../Estilos/PagCardsEstilo.css">
+
 <body>
-<div class="card-container">
-        <div class="card">
-            <div class="content">
-                <div class="front">
-                    <!-- Dados do banco de dados: imagem e nome da árvore -->
-                    <img src="imagem-arvore1.jpg" alt="Árvore 1">
-                    <h3>Nome da Árvore 1</h3>
-                </div>
-            </div>
-        </div>
+<?php include '../includes/header.php'; ?>
 
-        <div class="card">
-            <div class="content">
-                <div class="front">
-                    <!-- Dados do banco de dados: imagem e nome da árvore -->
-                    <img src="imagem-arvore2.jpg" alt="Árvore 2">
-                    <h3>Nome da Árvore 2</h3>
-                </div>
-            </div>
-        </div>
-
-        <div class="card">
-            <div class="content">
-                <div class="front">
-                    <!-- Dados do banco de dados: imagem e nome da árvore -->
-                    <img src="imagem-arvore3.jpg" alt="Árvore 3">
-                    <h3>Nome da Árvore 3</h3>
-                </div>
-            </div>
-        </div>
-
-        
-
-     
-
-    </div>
+<div class="cards-section">
     <div class="card-container">
         <div class="card">
             <div class="content">
                 <div class="front">
-                    <!-- Dados do banco de dados: imagem e nome da árvore -->
                     <img src="imagem-arvore1.jpg" alt="Árvore 1">
                     <h3>Nome da Árvore 1</h3>
                 </div>
@@ -57,7 +21,6 @@ include '../includes/header.php';
         <div class="card">
             <div class="content">
                 <div class="front">
-                    <!-- Dados do banco de dados: imagem e nome da árvore -->
                     <img src="imagem-arvore2.jpg" alt="Árvore 2">
                     <h3>Nome da Árvore 2</h3>
                 </div>
@@ -67,17 +30,42 @@ include '../includes/header.php';
         <div class="card">
             <div class="content">
                 <div class="front">
-                    <!-- Dados do banco de dados: imagem e nome da árvore -->
                     <img src="imagem-arvore3.jpg" alt="Árvore 3">
                     <h3>Nome da Árvore 3</h3>
                 </div>
             </div>
         </div>
-
-        
-
-     
-
     </div>
-</body>
+
+    <div class="card-container">
+        <div class="card">
+            <div class="content">
+                <div class="front">
+                    <img src="imagem-arvore1.jpg" alt="Árvore 1">
+                    <h3>Nome da Árvore 1</h3>
+                </div>
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="content">
+                <div class="front">
+                    <img src="imagem-arvore2.jpg" alt="Árvore 2">
+                    <h3>Nome da Árvore 2</h3>
+                </div>
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="content">
+                <div class="front">
+                    <img src="imagem-arvore3.jpg" alt="Árvore 3">
+                    <h3>Nome da Árvore 3</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 <?php include '../includes/footer.php'; ?>
+</body>
